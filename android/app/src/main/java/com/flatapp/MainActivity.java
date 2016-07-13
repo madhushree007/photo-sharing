ee68534c67a708f61f12a9545b99bba04ac832d4
Himanshu <himanshu@Himanshus-MacBook-Air-2.local>
2016-07-13 18:51:10 +0530

    merged upstream

--- android/app/src/main/java/com/flatapp/MainActivity.java
+++ android/app/src/main/java/com/flatapp/MainActivity.java
@@ -1,4 +1,4 @@
-package com.flatappseed;
+package com.flatapp;
 
 import com.facebook.react.ReactActivity;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -17,7 +17,7 @@ public class MainActivity extends ReactActivity {
      */
     @Override
     protected String getMainComponentName() {
-        return "FlatAppSeed";
+        return "FlatApp";
     }
 
     /**
