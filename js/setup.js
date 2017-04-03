8a4bd3cd42fb2b282190c562ca100ee0054aff87
Rishabh Pandey <rishabh@sahusoft.com>
2017-04-03 15:14:26 +0530

    fix(*): updated appicon

--- js/setup.js
+++ js/setup.js
@@ -2,6 +2,7 @@ import * as Expo from 'expo';
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import { StyleProvider } from 'native-base';
+
 import App from './App';
 import configureStore from './configureStore';
 import getTheme from '../native-base-theme/components';
